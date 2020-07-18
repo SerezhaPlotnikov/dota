a7e16964d5b447784d91a01bd8317c9dedf18369
SerezhaPlotnikov <frymeabeaver@gmail.com>
2020-07-18 15:47:25 +0300

    table

---
+++ src/components/heroes/HeroInfoBox.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const HeroInfoBox = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`;
+
+export const HeroInfo = styled.div``;
